eab86a837228c4e0db6ede6b44d434be1a973b8d
Isaac Abraham <isaac.abraham@gmail.com>
2013-10-13 16:57:50 +0100

    Small refactoring.

--- Elastacloud.AzureTypeProvider/AzureTypeProvider.fs
+++ Elastacloud.AzureTypeProvider/AzureTypeProvider.fs
@@ -9,50 +9,40 @@ open System.Reflection
 [<TypeProvider>]
 type AzureAccountTypeProvider() as this = 
     inherit TypeProviderForNamespaces()
-
     let namespaceName = "Elastacloud.FSharp.AzureTypeProvider"
     let thisAssembly = Assembly.GetExecutingAssembly()
-
-    let buildConnectionString (args:obj[]) =
+    
+    let buildConnectionString (args : obj []) = 
         let accountName = args.[0] :?> string
         let accountKey = args.[1] :?> string
-
         if accountName = null || accountKey = null then "UseDevelopmentStorage=true"
-        else sprintf "DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;" accountName accountKey
-
-    let buildTypes (typeName:string) (args:obj[]) =
+        else 
+            sprintf "DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;" accountName 
+                accountKey
+    
+    let buildTypes (typeName : string) (args : obj []) = 
         let containerListingType = ProvidedTypeDefinition("Containers", Some typeof<obj>)
-
         let connectionAccountType = 
             ProvidedTypeDefinition
                 (thisAssembly, namespaceName, typeName, baseType = Some typeof<obj>)
-
         let connectionString = buildConnectionString args
         connectionAccountType.AddMember
             (ProvidedConstructor(parameters = [], InvokeCode = (fun args -> <@@ null @@>)))
         connectionAccountType.AddMember(containerListingType)
-        
-        containerListingType.AddMembersDelayed(fun _ ->
+        containerListingType.AddMembersDelayed
+            (fun _ -> 
             AzureRepository.getBlobStorageAccountManifest (connectionString) 
-            |> List.map (fun container -> 
-                    let individualContainerType = ProvidedTypeDefinition(container.Name, Some typeof<obj>)
-                    individualContainerType.AddMembersDelayed(fun _ ->
-                        container.GetFiles()
-                        |> Seq.map (fun file -> ProvidedProperty(propertyName = file, propertyType = typeof<obj>, IsStatic = true, GetterCode = (fun args -> <@@ null @@>)))
-                        |> Seq.toList)
-                    individualContainerType)
-        )
-
+            |> List.map ContainerTypeFactory.createContainerType)
         connectionAccountType
-
-    let parameters = [
-        ProvidedStaticParameter("accountName", typeof<string>, String.Empty)
-        ProvidedStaticParameter("accountKey", typeof<string>, String.Empty)
-    ]
+    
+    let parameters = 
+        [ ProvidedStaticParameter("accountName", typeof<string>, String.Empty)
+          ProvidedStaticParameter("accountKey", typeof<string>, String.Empty) ]
+    
     let azureAccountType = 
         ProvidedTypeDefinition
             (thisAssembly, namespaceName, "AzureAccount", baseType = Some typeof<obj>)
-    do azureAccountType.DefineStaticParameters(parameters, buildTypes)   
+    do azureAccountType.DefineStaticParameters(parameters, buildTypes)
     do this.AddNamespace(namespaceName, [ azureAccountType ])
 
 [<TypeProviderAssembly>]
--- Elastacloud.AzureTypeProvider/ContainerTypeFactory.fs
+++ Elastacloud.AzureTypeProvider/ContainerTypeFactory.fs
@@ -7,21 +7,16 @@ open Microsoft.FSharp.Core.CompilerServices
 open Microsoft.FSharp.Quotations
 open Elastacloud.FSharp.AzureTypeProvider.AzureRepository
 
-let private createFileType fileName =
-    ProvidedProperty(propertyName = fileName,
-                     propertyType = typeof<string>,
-                     IsStatic = false,
-                     GetterCode = (fun args -> <@@ fileName @@>))
+let private createFileProperty fileName = 
+    ProvidedProperty
+        (propertyName = fileName, propertyType = typeof<obj>, IsStatic = true, 
+         GetterCode = (fun args -> <@@ null @@>))
 
 /// Generates a property type for a specific container
-let createContainerType(assembly,namespaceName,(container:LightweightContainer)) =
-    let containerType = ProvidedTypeDefinition(assembly, namespaceName, "ContainerFileListing", baseType = Some typeof<obj>)
-    containerType.AddMembers(container.GetFiles() |> Seq.map createFileType |> Seq.toList)
-    let containerName = container.Name
-    let property = ProvidedProperty(propertyName = container.Name,
-                                    propertyType = containerType,
-                                    IsStatic = false,
-                                    GetterCode = (fun args -> <@@ containerName @@>))
-
-    property, containerType
-
+let createContainerType (container : LightweightContainer) = 
+    let individualContainerType = ProvidedTypeDefinition(container.Name, Some typeof<obj>)
+    individualContainerType.AddMembersDelayed(fun _ -> 
+        container.GetFiles()
+        |> Seq.map createFileProperty
+        |> Seq.toList)
+    individualContainerType
