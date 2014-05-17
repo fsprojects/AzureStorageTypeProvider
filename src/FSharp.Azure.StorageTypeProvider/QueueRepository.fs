b6189cfcac30accff3ff3af32da40f6d93405559
Isaac Abraham <isaac.abraham@gmail.com>
2014-05-17 15:07:57 +0100

    Queue Repository.

---
+++ src/FSharp.Azure.StorageTypeProvider/QueueRepository.fs
@@ -0,0 +1,11 @@
+﻿module internal FSharp.Azure.StorageTypeProvider.Queue.QueueRepository
+
+open Microsoft.WindowsAzure.Storage
+open Microsoft.WindowsAzure.Storage.Queue.Protocol
+
+let private getQueueClient connection = CloudStorageAccount.Parse(connection).CreateCloudQueueClient()
+
+let getQueues connectionString =
+    getQueueClient(connectionString).ListQueues()
+    |> Seq.map(fun q -> q.Name)
+    |> Seq.toArray
