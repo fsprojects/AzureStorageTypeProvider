ac9384dfe166072ff2d01f654eb6d9589a87f85e
isaacabraham <isaac.abraham@gmail.com>
2014-06-09 22:30:01 +0100

    Some automated tests that run against local development storage.

---
+++ UnitTests/Script.fsx
@@ -0,0 +1,51 @@
+﻿// This script sets up local azure storage to a well-known state for tests.
+#r @"..\packages\WindowsAzure.Storage.3.0.2.0\lib\net40\Microsoft.WindowsAzure.Storage.dll"
+
+open Microsoft.WindowsAzure.Storage
+open Microsoft.WindowsAzure.Storage.Blob
+open Microsoft.WindowsAzure.Storage.Table
+open System
+
+// blob data
+let blobClient = CloudStorageAccount.DevelopmentStorageAccount.CreateCloudBlobClient()
+let container = blobClient.GetContainerReference("tp-test")
+
+if container.Exists() then container.Delete()
+container.Create()
+
+let createBlob fileName contents = 
+    let blob = container.GetBlockBlobReference(fileName)
+    blob.UploadText(contents)
+
+createBlob "file1.txt" "stuff"
+createBlob "file2.txt" "stuff"
+createBlob "file3.txt" "stuff"
+createBlob "folder/childFile.txt" "stuff"
+createBlob "sample.txt" "the quick brown fox jumped over the lazy dog
+bananas"
+createBlob "data.xml" "<data><items><item>thing</item></items></data>"
+
+// table data
+let tableClient = CloudStorageAccount.DevelopmentStorageAccount.CreateCloudTableClient()
+let table = tableClient.GetTableReference("tptest")
+
+type RandomEntity() = 
+    inherit TableEntity()
+    member val Name = String.Empty with get, set
+    member val Count = 0 with get, set
+    member val Dob = DateTime.MinValue with get, set
+
+let insertRow (name, count, dob) = 
+    RandomEntity(Name = name, Count = count, Dob = dob, PartitionKey = name, RowKey = count.ToString())
+    |> TableOperation.Insert
+    |> table.Execute
+
+if table.Exists() then table.Delete()
+table.Create()
+
+insertRow("fred", 10, DateTime(1990, 5, 1))
+insertRow("fred", 35, DateTime(1980, 4, 4))
+insertRow("tim", 99, DateTime(2001, 10, 5))
+insertRow("sara", 35, DateTime(2005, 4, 30))
+insertRow("rachel", 20, DateTime(1965, 8, 20))
+
---
+++ UnitTests/TableUnitTests.fs
@@ -0,0 +1,89 @@
+﻿module FSharp.Azure.StorageTypeProvider.``Table Unit Tests``
+
+open FSharp.Azure.StorageTypeProvider
+open FSharp.Azure.StorageTypeProvider.Table
+open Xunit
+open System
+open System.Linq
+
+type Local = AzureTypeProvider<"DevStorageAccount", "">
+
+let table = Local.Tables.tptest
+
+[<Fact>]
+let ``Correctly identifies tables``() =
+    // compiles!
+    Local.Tables.tptest
+
+[<Fact>]
+let ``Table name is correctly identified``() =
+    Assert.Equal<string>("tptest", table.Name)
+
+[<Fact>]
+let ``Matching row and partition key returns Some row``() =
+    match table.Get(Row "35", Partition "fred") with
+    | Some row -> Assert.Equal<string>("fred", row.Name)
+                  Assert.Equal(35, row.Count)
+                  Assert.Equal(DateTime(1980, 4, 4), row.Dob)
+    | None -> failwith "could not locate row"
+
+[<Fact>]
+let ``Non matching partition key returns None``() =
+    match table.Get(Row "35", Partition "random") with
+    | Some _ -> failwith "located a row that shouldn't have"
+    | None -> ()
+
+
+[<Fact>]
+let ``Non matching row key returns None``() =
+    match table.Get(Row "random", Partition "fred") with
+    | Some _ -> failwith "located a row that shouldn't have"
+    | None -> ()
+
+[<Fact>]
+let ``Single match on Row Key returns Some row``() =
+    match table.Get(Row "99") with
+    | Some row -> ()
+    | None -> failwith "could not locate row"
+
+[<Fact>]
+let ``Duplicate match on Row Key throws an exception``() =
+    Assert.Throws<Exception>(fun () -> table.Get(Row "35") |> ignore)
+
+[<Fact>]
+let ``Gets all rows in a table``() =
+    Assert.Equal(5, table.Query().Execute().Length)
+
+[<Fact>]
+let ``Gets all rows in a partition``() =
+    Assert.Equal(2, table.GetPartition("fred").Length)
+
+type MatchingTableRow =
+    { Name : string
+      Count : int 
+      Dob : DateTime }
+
+[<Fact>]
+let ``Inserts and deletes a row using lightweight syntax correctly``() =
+    let result = table.Insert(Partition "isaac", Row "500", { Name = "isaac"; Count = 500; Dob = DateTime.Now })
+    match result with
+    | SuccessfulResponse ((Partition "isaac", Row "500"), 204) ->
+        let deleteResponse = table.Delete([ Partition "isaac", Row "500"] )
+        match deleteResponse with
+        | [| "isaac", [| SuccessfulResponse((Partition "isaac", Row "500"), 204) |] |] -> ()
+        | _ -> failwith "error deleting"
+    | _ -> failwith "error inserting"
+    
+[<Fact>]
+let ``Inserts and deletes a batch on same partition using lightweight syntax correctly``() =
+    let result = table.Insert( [ Partition "isaac", Row "500", { Name = "isaac"; Count = 500; Dob = DateTime.Now }
+                                 Partition "isaac", Row "250", { Name = "isaac"; Count = 250; Dob = DateTime.Now }
+                               ])
+    match result with
+    | [| "isaac", [| SuccessfulResponse _; SuccessfulResponse _ |] |] ->
+        let deleteResponse = table.Delete([ Partition "isaac", Row "500"; Partition "isaac", Row "250" ] )
+        match deleteResponse with
+        | [| "isaac", [| SuccessfulResponse _; SuccessfulResponse _ |] |] -> ()
+        | _ -> failwith "error deleting"
+    | _ -> failwith "error inserting"
+
