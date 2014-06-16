dc673d382ff195de4791cc72808cf9c3014fb318
isaacabraham <isaac.abraham@gmail.com>
2014-06-16 10:34:49 +0100

    Made Get by Row and Partition Key more consistent.

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -10,5 +10,8 @@
 * Removed EntityId from the LightweightTableEntity and replaced with separate PartitionKey and RowKey properties
 * LightweightTableEntity made public and changed to a class rather than a record
 
-### 0.9.2 - Unreleased
+### 0.9.2 - 12th June 2014
 * Fixed a bug whereby provided type constructors sometimes mixed up field names and their values.
+
+### 1.0.0 - 14th June 2014
+* Forced Get to provide a Partition Key as well as Row Key to semantically reflect that it can never return more than 1 result (this removes the exception path).
--- src/FSharp.Azure.StorageTypeProvider/AssemblyInfo.fs
+++ src/FSharp.Azure.StorageTypeProvider/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Azure.StorageTypeProvider")>]
 [<assembly: AssemblyProductAttribute("FSharp.Azure.StorageTypeProvider")>]
 [<assembly: AssemblyDescriptionAttribute("Allows easy access to Azure Storage assets through F# scripts.")>]
-[<assembly: AssemblyVersionAttribute("0.9.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.9.2")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.9.2"
+    let [<Literal>] Version = "1.0.0"
