c99704484c2357c6bb1548d8b4bbc212505e2c2b
isaacabraham <isaac.abraham@gmail.com>
2015-04-18 12:05:58 +0100

    Some tests for connection string formats.

---
+++ tests/IntegrationTests/ConnectionTests.fs
@@ -0,0 +1,27 @@
+﻿module ConnectionTests
+
+open FSharp.Azure.StorageTypeProvider
+open Swensen.Unquote
+open Xunit
+
+type SecondBlank = AzureTypeProvider<"Foo", "">
+type FirstBlank = AzureTypeProvider<"", "Foo">
+//type TwoPart = AzureTypeProvider<"devstoreaccount1", "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==">
+//type FullPath = AzureTypeProvider<"DefaultEndpointsProtocol=https;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;", "test">
+
+[<Fact>]
+let ``SecondBlank connection string works``() =
+    SecondBlank.Containers.CloudBlobClient.GetContainerReference("tptest").Exists() =? true
+
+[<Fact>]
+let ``FirstBlank connection string works``() =
+    FirstBlank.Containers.CloudBlobClient.GetContainerReference("tptest").Exists() =? true
+
+//[<Fact>]
+//let ``TwoPart connection string works``() =
+//    TwoPart.Containers.CloudBlobClient.GetContainerReference("tptest").Exists() =? true
+//
+//[<Fact>]
+//let ``FullPath connection string works``() =
+//    FullPath.Containers.CloudBlobClient.GetContainerReference("tptest").Exists() =? true
+//
