120042f6992c55b5879cc11ea9177078f7e6dcec
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-08-17 22:30:41 +0200

    Trying to update the build script

--- build.fsx
+++ build.fsx
@@ -1,22 +1,28 @@
 // --------------------------------------------------------------------------------------
-// FAKE build script 
+// FAKE build script
 // --------------------------------------------------------------------------------------
-#r "packages/FAKE/tools/FakeLib.dll"
+
+#r @"packages/FAKE/tools/FakeLib.dll"
 
 open Fake
-open Fake.AssemblyInfoFile
 open Fake.Git
+open Fake.AssemblyInfoFile
 open Fake.ReleaseNotesHelper
+open Fake.UserInputHelper
 open System
 open System.IO
 
 // --------------------------------------------------------------------------------------
+// START TODO: Provide project-specific details below
+// --------------------------------------------------------------------------------------
+
 // Information about the project are used
 //  - for version and project name in generated AssemblyInfo file
-//  - by the generated NuGet package 
+//  - by the generated NuGet package
 //  - to run tests and to publish documentation on GitHub gh-pages
 //  - for documentation, you also need to edit info in "docs/tools/generate.fsx"
-// The name of the project 
+
+// The name of the project
 // (used by attributes in AssemblyInfo, name of a NuGet package and directory in 'src')
 let project = "FSharp.Azure.StorageTypeProvider"
 // Short summary of the project
@@ -34,39 +40,64 @@ let tags = "azure, f#, fsharp, type provider, blob, table, queue, script"
 let solutionFile = "UnitTests"
 // Pattern specifying assemblies to be tested using XUnit
 let testAssemblies = "tests/**/bin/Release/*Tests*.dll"
+
 // Git configuration (used for publishing documentation in gh-pages branch)
-// The profile where the project is posted 
-let gitHome = "https://github.com/fsprojects"
+// The profile where the project is posted
+let gitOwner = "fsprojects"
+let gitHome = "https://github.com/" + gitOwner
+
 // The name of the project on GitHub
 let gitName = "AzureStorageTypeProvider"
 
+// The url for the raw files hosted
+let gitRaw = environVarOrDefault "gitRaw" "https://raw.github.com/fsprojects"
+
 // --------------------------------------------------------------------------------------
-// END TODO: The rest of the file includes standard build steps 
+// END TODO: The rest of the file includes standard build steps
 // --------------------------------------------------------------------------------------
 // Read additional information from the release notes document
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let release = parseReleaseNotes (File.ReadAllLines "RELEASE_NOTES.md")
+let buildDir = "bin"
+let tempDir = "temp"
+
+// Read additional information from the release notes document
+let releaseNotesData = 
+    File.ReadAllLines "RELEASE_NOTES.md"
+    |> parseAllReleaseNotes
+
+let release = List.head releaseNotesData
 
 // Generate assembly info files with the right version & up-to-date information
-Target "AssemblyInfo" (fun _ -> 
+Target "AssemblyInfo" (fun _ ->
     let fileName = "src/" + project + "/AssemblyInfo.fs"
     CreateFSharpAssemblyInfo fileName [ Attribute.Title project
                                         Attribute.Product project
                                         Attribute.Description summary
                                         Attribute.Version release.AssemblyVersion
-                                        Attribute.FileVersion release.AssemblyVersion ])
+                                        Attribute.FileVersion release.AssemblyVersion ]
+)
+
 // --------------------------------------------------------------------------------------
-// Clean build results & restore NuGet packages
+// Clean build results
 Target "Clean" (fun _ -> CleanDirs [ "bin"; "temp"; "tests/integrationtests/bin" ])
 
+Target "CleanDocs" (fun _ ->
+    CleanDirs ["docs/output"]
+)
+
+// --------------------------------------------------------------------------------------
 // Build library & test project
-Target "Build" (fun _ -> 
+
+Target "Build" (fun _ ->
     !!("*.sln")
     |> MSBuildRelease "" "Rebuild"
-    |> ignore)
+    |> ignore
+)
+
 // --------------------------------------------------------------------------------------
-// Testing
+// Run the unit tests using test runner
+
 Target "ResetTestData" (fun _ ->
     let emulatorPath =
         [ "AzureStorageEmulator"; "WAStorageEmulator" ]
@@ -83,38 +114,16 @@ Target "ResetTestData" (fun _ ->
     |> snd
     |> Seq.iter(fun x -> printfn "%s" x.Message)
 )
+
 // Run integration tests
-Target "IntegrationTests" (fun _ ->
+Target "RunTests" (fun _ ->
     !!(testAssemblies)
-    |> xUnit (fun p -> { p with Verbose = true }))
-// --------------------------------------------------------------------------------------
-// Generate the documentation
-Target "CleanDocs" (fun _ -> CleanDirs [ "docs/output" ])
-Target "GenerateReferenceDocs" (fun _ ->
-    if not <| executeFSIWithArgs "docs/tools" "generate.fsx" ["--define:RELEASE"; "--define:REFERENCE"] [] then
-      failwith "generating reference documentation failed"
+    |> xUnit (fun p -> { p with Verbose = true })
 )
 
-Target "GenerateHelp" (fun _ ->
-    if not <| executeFSIWithArgs "docs/tools" "generate.fsx" ["--define:RELEASE"; "--define:HELP"] [] then
-      failwith "generating help documentation failed"
-)
-
-Target "GenerateDocs" DoNothing
-
-Target "ReleaseDocs" (fun _ ->
-    let tempDocsDir = "temp/gh-pages"
-    CleanDir tempDocsDir
-    Repository.cloneSingleBranch "" (gitHome + "/" + gitName + ".git") "gh-pages" tempDocsDir
-
-    CopyRecursive "docs/output" tempDocsDir true |> tracefn "%A"
-    StageAll tempDocsDir
-    Commit tempDocsDir (sprintf "Update generated documentation for version %s" release.NugetVersion)
-    Branches.push tempDocsDir
-)
 // --------------------------------------------------------------------------------------
 // Build a NuGet package
-Target "Package" 
+Target "NuGet" 
     (fun _ -> 
         NuGet (fun p ->
             { p with Authors = authors
@@ -135,40 +144,164 @@ Target "Package"
                             "Newtonsoft.Json.dll"; "System.Spatial.dll" ] 
                           |> List.map (fun file -> @"..\bin\" + file, Some "lib/net40", None))
                           @ [ "StorageTypeProvider.fsx", None, None ] }) 
-              ("nuget/" + project + ".nuspec"))
+              ("nuget/" + project + ".nuspec")
+)
+
+
+// --------------------------------------------------------------------------------------
+// Generate the documentation
+
+Target "GenerateReferenceDocs" (fun _ ->
+    if not <| executeFSIWithArgs "docs/tools" "generate.fsx" ["--define:RELEASE"; "--define:REFERENCE"] [] then
+      failwith "generating reference documentation failed"
+)
+
+let generateHelp fail debug =
+    let args =
+        [ if not debug then yield "--define:RELEASE"
+          yield "--define:HELP"]
+
+    if executeFSIWithArgs "docs/tools" "generate.fsx" args [] then
+        traceImportant "Help generated"
+    else
+        if fail then
+            failwith "generating help documentation failed"
+        else
+            traceImportant "generating help documentation failed"
+
+Target "GenerateHelp" (fun _ ->
+    DeleteFile "docs/content/release-notes.md"
+    CopyFile "docs/content/" "RELEASE_NOTES.md"
+    Rename "docs/content/release-notes.md" "docs/content/RELEASE_NOTES.md"
+
+    DeleteFile "docs/content/license.md"
+    CopyFile "docs/content/" "LICENSE.txt"
+    Rename "docs/content/license.md" "docs/content/LICENSE.txt"
+
+    CopyFile buildDir "packages/FSharp.Core/lib/net40/FSharp.Core.sigdata"
+    CopyFile buildDir "packages/FSharp.Core/lib/net40/FSharp.Core.optdata"
+
+    generateHelp true true
+)
+
+Target "GenerateHelpDebug" (fun _ ->
+    DeleteFile "docs/content/release-notes.md"
+    CopyFile "docs/content/" "RELEASE_NOTES.md"
+    Rename "docs/content/release-notes.md" "docs/content/RELEASE_NOTES.md"
+
+    DeleteFile "docs/content/license.md"
+    CopyFile "docs/content/" "LICENSE.txt"
+    Rename "docs/content/license.md" "docs/content/LICENSE.txt"
+
+    generateHelp true true
+)
+
+Target "KeepRunning" (fun _ ->    
+    use watcher = !! "docs/content/**/*.*" |> WatchChanges (fun changes ->
+         generateHelp false false
+    )
+
+    traceImportant "Waiting for help edits. Press any key to stop."
+
+    System.Console.ReadKey() |> ignore
+
+    watcher.Dispose()
+)
+
+Target "GenerateDocs" DoNothing
+
+// --------------------------------------------------------------------------------------
+// Release Scripts
+
+Target "ReleaseDocs" (fun _ ->
+    let tempDocsDir = "temp/gh-pages"
+    CleanDir tempDocsDir
+    Repository.cloneSingleBranch "" (gitHome + "/" + gitName + ".git") "gh-pages" tempDocsDir
+
+    Git.CommandHelper.runSimpleGitCommand tempDocsDir "rm . -f -r" |> ignore
+    CopyRecursive "docs/output" tempDocsDir true |> tracefn "%A"    
+    
+    StageAll tempDocsDir
+    Git.Commit.Commit tempDocsDir (sprintf "Update generated documentation for version %s" release.NugetVersion)
+    Branches.push tempDocsDir
+)
+
+#load "paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx"
+open Octokit
+
+Target "Release" (fun _ ->
+    let user =
+        match getBuildParam "github-user" with
+        | s when not (String.IsNullOrWhiteSpace s) -> s
+        | _ -> getUserInput "Username: "
+    let pw =
+        match getBuildParam "github-pw" with
+        | s when not (String.IsNullOrWhiteSpace s) -> s
+        | _ -> getUserPassword "Password: "
+    let remote =
+        Git.CommandHelper.getGitResult "" "remote -v"
+        |> Seq.filter (fun (s: string) -> s.EndsWith("(push)"))
+        |> Seq.tryFind (fun (s: string) -> s.Contains(gitOwner + "/" + gitName))
+        |> function None -> gitHome + "/" + gitName | Some (s: string) -> s.Split().[0]
+
+    StageAll ""
+    Git.Commit.Commit "" (sprintf "Bump version to %s" release.NugetVersion)
+    Branches.pushBranch "" remote (Information.getBranchName "")
+
+    Branches.tag "" release.NugetVersion
+    Branches.pushTag "" remote release.NugetVersion
+    
+    // release on github
+    createClient user pw
+    |> createDraft gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes
+    |> releaseDraft
+    |> Async.RunSynchronously
+)
+
+
 Target "LocalDeploy" (fun _ ->
     directoryInfo @"bin"
     |> filesInDirMatching "*.nupkg"
     |> Seq.map(fun x -> x.FullName)
     |> CopyFiles "..\..\LocalPackages")
 
+Target "BuildPackage" DoNothing
+
 // --------------------------------------------------------------------------------------
 // Run all targets by default. Invoke 'build <Target>' to override
-Target "All" DoNothing
-Target "Release" DoNothing
-Target "TestBuild" DoNothing
-Target "PackageForRelease" DoNothing
 
-"Build"
-  ==> "CleanDocs"
-  ==> "GenerateHelp"
-  ==> "GenerateReferenceDocs"
-  ==> "GenerateDocs"
-  ==> "ReleaseDocs"
-  ==> "Release"
+Target "All" DoNothing
 
 "Clean"
   ==> "AssemblyInfo"
   ==> "ResetTestData"
   ==> "Build"
-  ==> "IntegrationTests"
-  ==> "GenerateDocs"
-  ==> "PackageForRelease"
+  ==> "RunTests"
+  =?> ("GenerateReferenceDocs",isLocalBuild && not isMono)
+  =?> ("GenerateDocs",isLocalBuild && not isMono)
   ==> "All"
+  =?> ("ReleaseDocs",isLocalBuild && not isMono)
 
-"Build"
-  ==> "Package"
+"All"
+  ==> "NuGet"
   ==> "LocalDeploy"
-  ==> "PackageForRelease"
+  ==> "BuildPackage"
 
-RunTargetOrDefault "LocalDeploy"
+"CleanDocs"
+  ==> "GenerateHelp"
+  ==> "GenerateReferenceDocs"
+  ==> "GenerateDocs"
+
+"CleanDocs"
+  ==> "GenerateHelpDebug"
+
+"GenerateHelp"
+  ==> "KeepRunning"
+    
+"ReleaseDocs"
+  ==> "Release"
+
+"BuildPackage"
+  ==> "Release"
+
+RunTargetOrDefault "All"
