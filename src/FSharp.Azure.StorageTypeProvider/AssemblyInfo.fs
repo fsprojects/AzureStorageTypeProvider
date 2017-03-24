c122db9cf5b137fed99e105cb8c161fe8cea4452
Isaac Abraham <isaac.abraham@gmail.com>
2017-03-24 17:30:39 +0100

    Upgrade to 1.8.0

--- src/FSharp.Azure.StorageTypeProvider/AssemblyInfo.fs
+++ src/FSharp.Azure.StorageTypeProvider/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Azure.StorageTypeProvider")>]
 [<assembly: AssemblyProductAttribute("FSharp.Azure.StorageTypeProvider")>]
 [<assembly: AssemblyDescriptionAttribute("Allows easy access to Azure Storage assets through F# scripts.")>]
-[<assembly: AssemblyVersionAttribute("1.7.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.7.0")>]
+[<assembly: AssemblyVersionAttribute("1.8.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.8.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.Azure.StorageTypeProvider"
     let [<Literal>] AssemblyProduct = "FSharp.Azure.StorageTypeProvider"
     let [<Literal>] AssemblyDescription = "Allows easy access to Azure Storage assets through F# scripts."
-    let [<Literal>] AssemblyVersion = "1.7.0"
-    let [<Literal>] AssemblyFileVersion = "1.7.0"
+    let [<Literal>] AssemblyVersion = "1.8.0"
+    let [<Literal>] AssemblyFileVersion = "1.8.0"
