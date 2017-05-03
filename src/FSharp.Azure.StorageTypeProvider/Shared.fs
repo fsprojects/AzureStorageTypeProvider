dc01632c0db982bc5cf3afcbf59244a98c19c4b7
Prashant Pathak <theprash@gmail.com>
2017-05-03 17:29:26 +0100

    More flexible JSON parsing and more concise table schema

--- src/FSharp.Azure.StorageTypeProvider/Shared.fs
+++ src/FSharp.Azure.StorageTypeProvider/Shared.fs
@@ -26,3 +26,43 @@ module Async =
         async {
             let! result = workflow
             return mapper result }
+
+///[omit]
+[<RequireQualifiedAccess>]
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Json =
+    open Newtonsoft.Json.Linq
+
+    type Json =
+        | String of string
+        | Number of decimal
+        | Boolean of bool
+        | Object of (string * Json)[]
+        | Array of Json[]
+        | Null  
+        member this.AsString = match this with Json.String s -> s | _ -> failwith "Not a string"
+        member this.AsNumber = match this with Json.Number s -> s | _ -> failwith "Not a number"
+        member this.AsBoolean = match this with Json.Boolean b -> b | _ -> failwith "Not a boolean"
+        member this.AsObject = match this with Json.Object o -> o | _ -> failwith "Not an object"
+        member this.AsArray = match this with Json.Array o -> o | _ -> failwith "Not an array"
+        member this.GetProperty key = this.AsObject |> Array.find (fst >> (=) key) |> snd
+        member this.TryGetProperty key =
+            match this with Json.Object o -> Some o | _ -> None
+            |> Option.bind (Array.tryFind (fst >> (=) key))
+            |> Option.map snd
+
+    let rec ofJToken (jToken:JToken) =
+        match jToken with
+        | :? JValue as v ->
+            match v.Value with
+            | :? string as s -> Json.String s
+            | :? decimal as d -> Json.Number (decimal d)
+            | :? int64 as i -> Json.Number (decimal i)
+            | :? int32 as i -> Json.Number (decimal i)
+            | :? float as f -> Json.Number (decimal f)
+            | :? bool as b -> Json.Boolean b
+            | null -> Json.Null
+            | v -> failwithf "Cannot convert JSON value type %s" (v.GetType().FullName)
+        | :? JObject as o -> o.Properties() |> Seq.map (fun p -> p.Name, ofJToken p.Value) |> Seq.toArray |> Json.Object
+        | :? JArray as a -> a.Values() |> Seq.map ofJToken |> Seq.toArray |> Json.Array
+        | v -> failwithf "Cannot convert JSON type %s" (v.GetType().FullName)
