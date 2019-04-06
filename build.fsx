e3d1bf6bcbc9818506da6c7c25bad2475ac3ec6e
Isaac Abraham <isaac.abraham@gmail.com>
2019-04-06 23:52:50 +0100

    Update build.fsx

--- build.fsx
+++ build.fsx
@@ -138,8 +138,7 @@ Target.create "NuGet"
                 ReleaseNotes = release.Notes |> String.concat Environment.NewLine
                 Tags = "azure, f#, fsharp, type provider, blob, table, queue, script"
                 OutputPath = "bin"
-                Dependencies = [ "WindowsAzure.Storage", "9.3.2"
-                                 "FSharp.Compiler.Tools", "10.2.1" ]
+                Dependencies = [ "WindowsAzure.Storage", "9.3.2" ]
                 References = [ "FSharp.Azure.StorageTypeProvider.dll" ]
                 Files = 
                     ([ "FSharp.Azure.StorageTypeProvider.xml"; "FSharp.Azure.StorageTypeProvider.dll"
